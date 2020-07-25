7bbefcb8017a2a773f911299aeb29ec2ed5f0e69
Vitor B Appolinario <vitorappolinario@gmail.com>
2020-07-25 10:25:27 -0300

    Versão do final do video

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    const [newTodo, setNewTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+    const onNewTodoChange = useCallback((e) => {setNewTodo(e.target.value)}, []);
+
+    const formSubmitted = useCallback((e) => {
+        e.preventDefault();
+        if (!newTodo.trim()) return;
+        setTodos([
+            {
+                id: todos.length ? todos[0].id + 1 : 1,
+                content: newTodo,
+                done: false,
+            },
+            ...todos,
+        ]);
+        setNewTodo('');
+    }, [todos, newTodo]);
+
+    useEffect(() => {
+        console.log('todos', todos);
+    }, [todos]);
+
+    const addTodo = useCallback((todo, index) => (event) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1, {
+            ...todo,
+            done: !todo.done
+        });
+        setTodos(newTodos);
+    }, [todos]);
+
+    const removeTodo = useCallback((todo) => (event) => {
+        setTodos(todos.filter(otherTodo => otherTodo !== todo));
+    }, [todos]);
+
+    return (
+    <div>
+        <form onSubmit={formSubmitted}>
+            <label htmlFor="New todo">Enter task:</label>
+            <input
+                id="newTodo"
+                name="newTodo"
+                value={newTodo}
+                onChange={onNewTodoChange}
+            />
+            <button>Add Todo</button>
+        </form>
+        <ul>
+            {todos.map((todo, index) => (
+                <li key={todo.id} >
+                    <input
+                        checked={todo.done}
+                        type="checkbox"
+                        onChange={addTodo(todo, index)}
+                    />
+                    <span className={todo.done ? 'done' : ''}>{todo.content}</span>
+                    <button onClick={removeTodo(todo)}>Remove</button>
+                </li>
+            ))}
+        </ul>
     </div>
-  );
-}
+    );
+};
 
 export default App;
